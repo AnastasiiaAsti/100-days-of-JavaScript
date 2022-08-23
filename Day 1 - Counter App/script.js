0561bbe47acf1764297ef1c9c58057f01af92ae0
Asti <astistateofmind@gmail.com>
2022-08-23 13:59:44 -0700

    add changing color func

--- Day 1 - Counter App/script.js
+++ Day 1 - Counter App/script.js
@@ -7,12 +7,24 @@ const buttons = document.querySelector(".buttons");
 buttons.addEventListener("click", (evt) => {
     if (evt.target.classList.contains("add")) {
         count.innerHTML++;
+        setColor()
     }
     if (evt.target.classList.contains("subtract")) {
         count.innerHTML--;
+        setColor()
     }
     if (evt.target.classList.contains("reset")) {
         count.innerHTML = 0;
+        setColor()
     }
 })
 
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "yellow";
+    } else if (count.innerHTML < 0) {
+        count.style.color = "red";
+    } else {
+        count.style.color = "white";
+    }
+}
