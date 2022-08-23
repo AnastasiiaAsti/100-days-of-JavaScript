2c5c4d53ccfa4dc324f1b3865058cd5064acd0e3
Asti <astistateofmind@gmail.com>
2022-08-23 13:52:37 -0700

    update w evt delegation

--- Day 1 - Counter App/script.js
+++ Day 1 - Counter App/script.js
@@ -2,15 +2,24 @@ const count = document.querySelector(".count");
 const add = document.querySelector(".add");
 const resetCount = document.querySelector(".reset");
 const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
 
-add.addEventListener("click", () => {
-    count.innerHTML++;
+buttons.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("add")) {
+        console.log("add")
+    }
 })
 
-sub.addEventListener("click", () => {
-    count.innerHTML--;
-})
 
-resetCount.addEventListener("click", () => {
-    count.innerHTML = 0;
-})
+
+// add.addEventListener("click", () => {
+//     count.innerHTML++;
+// })
+
+// sub.addEventListener("click", () => {
+//     count.innerHTML--;
+// })
+
+// resetCount.addEventListener("click", () => {
+//     count.innerHTML = 0;
+// })
