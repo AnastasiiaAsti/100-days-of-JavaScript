682d4b2d063bc417c2d3e3cb8a51b1f08b33b767
Asti <astistateofmind@gmail.com>
2022-08-23 13:56:09 -0700

    update logic

--- Day 1 - Counter App/script.js
+++ Day 1 - Counter App/script.js
@@ -6,20 +6,13 @@ const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (evt) => {
     if (evt.target.classList.contains("add")) {
-        console.log("add")
+        count.innerHTML++;
+    }
+    if (evt.target.classList.contains("subtract")) {
+        count.innerHTML--;
+    }
+    if (evt.target.classList.contains("reset")) {
+        count.innerHTML = 0;
     }
 })
 
-
-
-// add.addEventListener("click", () => {
-//     count.innerHTML++;
-// })
-
-// sub.addEventListener("click", () => {
-//     count.innerHTML--;
-// })
-
-// resetCount.addEventListener("click", () => {
-//     count.innerHTML = 0;
-// })
