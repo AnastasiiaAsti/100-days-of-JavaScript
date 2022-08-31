081862d01a7dad35e30eef86078686e2572cffb6
Asti <astistateofmind@gmail.com>
2022-08-31 15:45:59 -0700

    add func to copy the generated link

--- Day 8 - Download link generator/script.js
+++ Day 8 - Download link generator/script.js
@@ -9,6 +9,7 @@ btn.addEventListener('click', generateLink);
 function generateLink(e) {
     e.preventDefault();
     // console.log(glink.value)
+    const copy = document.querySelector('.copy')
     const gLinkValue = gLink.value;
     const confirmLink = gLink.value.includes('https://drive.google.com/file/d/');
     // console.log(confirmLink)
@@ -16,5 +17,18 @@ function generateLink(e) {
         const getDownloadLink = gLinkValue.replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=").replace("/view?usp=sharing", "");
         // console.log(getDownloadLink)
         downloadLink.value = getDownloadLink;
+
+        function copyText(target) {
+            if (target.value === "") {
+                alert("Please generate a link")
+            } else {
+                target.select();
+                document.execCommand('copy');
+                copy.innerHTML = 'COPIED!';
+            }
+        }
+        copy.addEventListener('click', () => {
+            return copyText(downloadLink);
+        })
     }
 }
