8014b1fc42c0e4dc7fb92031b2dd5a7342875e30
Asti <astistateofmind@gmail.com>
2022-09-07 17:52:43 -0700

    add change color to the func

---
+++ Day 10 - Internet connection checker/script.js
@@ -0,0 +1,42 @@
+const image = document.getElementById("image");
+const statusDisplay = document.getElementById("status");
+const bgColor = document.getElementById("main");
+
+function setColor() {
+    bgColor.classList.add("online")
+}
+
+async function connectionStatus() {
+    try {
+        setColor();
+        return fetchRe
+    } catch (error) {
+        statusDisplay.textContent = "OOOOPSS!! Your Internet connection is down";
+        bgColor.classList.remove("online");
+    }
+}
+
+setInterval(async () => {
+    const result = await connectionStatus();
+    if (result) {
+        statusDisplay.textContent = "You are ONLINE! Connection looks good";
+        setColor();
+    }
+})
+
+
+        // window.addEventListener("load", (evt) => {
+        //     const statusDisplay = document.getElementById("status");
+        //     statusDisplay.textContent = navigator.online ? "Online" : "Offline";
+        // })
+
+        // window.addEventListener("offline", (evt) => {
+        //     const statusDisplay = document.getElementById("status");
+        //     statusDisplay.textContent = "Offline";
+        // })
+
+        // window.addEventListener("online", (evt) => {
+        //     const statusDisplay = document.getElementById("status");
+        //     section.classList.add(".online");
+        //     statusDisplay.textContent = "Online";
+        // })
