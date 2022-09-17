f62c159832d1dffd8ffee5cc4ef3d5f813240d81
Asti <astistateofmind@gmail.com>
2022-09-17 11:13:17 -0700

    add question class

--- Day 11 - Quiz/script.js
+++ Day 11 - Quiz/script.js
@@ -21,3 +21,15 @@ class Quiz {
 }
 
 //create question class
+
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
